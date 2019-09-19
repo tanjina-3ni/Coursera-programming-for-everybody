de3ddb9d69e92fdf07ae5f2ef000dc450093f1d5
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2019-09-20 01:43:38 +0600

    Assignment week-4

---
+++ courseraweek42.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 21:52:08 2019
+
+@author: Aspire
+"""
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Rate Per Hour:")
+r = float(rate)
+pay=h*r
+print pay
